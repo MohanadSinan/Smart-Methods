8846f986b0cefc497dde32e5461ea3909b4aeb56
Mohanad Sinan <mohanad14141@hotmail.com>
2020-07-06 07:30:04 +0300

    Control Panel V2

---
+++ Path4/Control Panel/script.js
@@ -0,0 +1,24 @@
+const mode = document.querySelector(".mode");
+let body = {
+  el: document.querySelector("body"),
+  dark: false
+};
+
+mode.addEventListener("click", () => {
+  if (body.dark === false) {
+    body.dark = true;
+    body.el.className = "dark";
+    mode.textContent = "dark";
+  } else {
+    body.dark = false;
+    body.el.className = "light";
+    mode.textContent = "light";
+  }
+});
+
+const btns = document.querySelectorAll("li > button");
+btns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    btn.classList.toggle("active");
+  });
+});
